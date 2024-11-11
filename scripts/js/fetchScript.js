9b9fdb0eb01eea768d130754b0feb9d470164253
sean <sean.li@vectorindex.cloud>
2024-11-10 18:46:04 -0800

    Fix typos

--- scripts/js/fetchScript.js
+++ scripts/js/fetchScript.js
@@ -4,13 +4,13 @@ async function fetchScript(remote, local) {
         local = remote
     }
     try {
-        let [remoteName, remoteExtension] = remote.split(".")
+        const [remoteName, remoteExtension] = remote.split(".")
         if (!remoteExtension in ["js", "py"]) {
             throw new Error("Only support .js and .py")
         }
-        const url = `https://raw.githubusercontent.com/yuxiaoli/terminal/refs/heads/main/scripts/${remoteExtension}/${remoteName}.${remoteExtension}`
+        const url = `https://raw.githubusercontent.com/Vector-Index/terminal/refs/heads/main/scripts/${remoteExtension}/${remoteName}.${remoteExtension}`
         const script = await window.utils.api.get(url)
-        const [localName, loalExtension] = local.split(".")
+        const [localName, localExtension] = local.split(".")
         if (localExtension !== remoteExtension) {
             throw new Error("Local script and remote script must have the same extension")
         }
@@ -21,4 +21,4 @@ async function fetchScript(remote, local) {
         return `Error: ${error}`
     }
 }
-fetchScript(kwargs.remote, kwargs.local)
+fetchScript(kwargs[0], kwargs[1])

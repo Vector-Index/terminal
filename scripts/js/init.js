3c5c7a54405d23487b5cb5a5b7e11ec690749d5d
sean <sean.li@vectorindex.cloud>
2024-11-10 19:05:18 -0800

    Fix typos

--- scripts/js/init.js
+++ scripts/js/init.js
@@ -1,13 +1,16 @@
 // Fetch config from remote URL
 async function init() {
     const url = `https://raw.githubusercontent.com/Vector-Index/terminal/refs/heads/main/configs/test.json`
-    const config = await window.utils.api.get(url)
+    const configuration = await window.utils.api.get(url)
+    console.log(configuration)
     // Replace/reload the current config
     try {
-    	config.value = JSON.parse(config)
-    	data = config.data
-    	scripts = config.scripts
+    	config.value = JSON.parse(configuration)
+    	data = config.value.data
+    	scripts = config.value.scripts
+    	return `Fetched config from ${url}`
 	} catch (error) {
 		return `Error: ${error}`
 	}
 }
+init()
